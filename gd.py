f4f6531a8064dd0b3f83aab1cb20f972580605d5
Aryan <146320254+aryancholakkal@users.noreply.github.com>
2024-07-31 00:10:40 +0530

    Update gd.py

--- gd.py
+++ gd.py
@@ -12,12 +12,10 @@ epochs =100
 
 for epoch in range(epochs):
   value = a*x**3 + b*x**2 + d*x +c
-  #Calculate the gradient of the equation with respect to x
   gradient  = 3*a *x**2 + 2*b*x+c
-  #Apply ReLU to the gradient (if it's negative)
   if gradient < -1:
     gradient =0
-    #Updating x using SGD
+    
     x -=  learning_rate *gradient
 
 print(f"The solution for x is approximately : {x}")
